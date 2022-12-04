bb706f1f4fb0858e7f48eb8ddf4e6bb7c6f939f4
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-12-04 08:58:31 +0200

    HW 11 draft

--- src/index.js
+++ src/index.js
@@ -10,17 +10,34 @@ const refs = {
   galleryWrap: document.querySelector('.gallery'),
 };
 
+let page = null;
+let query = null;
+
 const onFormSubmit = e => {
   e.preventDefault();
+  page = 1;
 
-  const query = e.target.elements.searchQuery.value.trim();
+  query = e.target.elements.searchQuery.value.trim();
 
   axios
     .get(
-      `https://pixabay.com/api/?key=31815472-6ffac1728c09639d971d276fb&q=${query}&image-type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=31815472-6ffac1728c09639d971d276fb&q=${query}&image-type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+    )
+    .then(r => {
+      clearMarkup();
+      renderPhotos(r.data.hits);
+      page += 1;
+    });
+};
+
+const onLoadMoreClick = () => {
+  axios
+    .get(
+      `https://pixabay.com/api/?key=31815472-6ffac1728c09639d971d276fb&q=${query}&image-type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     )
     .then(r => {
       renderPhotos(r.data.hits);
+      page += 1;
     });
 };
 
@@ -29,8 +46,6 @@ const clearMarkup = () => {
 };
 
 const renderPhotos = photos => {
-  clearMarkup();
-
   if (photos.length === 0) {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -44,16 +59,20 @@ const renderPhotos = photos => {
   <a href="${largeImageURL}"><img src="${webformatURL}" alt="" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
-      <b>Likes ${likes}</b>
+      <b>Likes</b>
+      <span>${likes}</span>
     </p>
     <p class="info-item">
-      <b>Views ${views}</b>
+      <b>Views</b>
+      <span>${views}</span>
     </p>
     <p class="info-item">
-      <b>Comments ${comments}</b>
+      <b>Comments</b>
+      <span>${comments}</span>
     </p>
     <p class="info-item">
-      <b>Downloads ${downloads}</b>
+      <b>Downloads</b>
+      <span>${downloads}</span>
     </p>
   </div>
 </div>`
@@ -61,7 +80,15 @@ const renderPhotos = photos => {
 
   refs.galleryWrap.insertAdjacentHTML('beforeend', markup.join(''));
 
+  showLoadMoreBtn();
+
   const lightbox = new SimpleLightbox('.gallery a');
 };
 
+const showLoadMoreBtn = () => {
+  refs.loadMoreBtn.classList.remove('hidden');
+};
+
 refs.searchForm.addEventListener('submit', onFormSubmit);
+
+refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
