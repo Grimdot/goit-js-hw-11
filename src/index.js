fbb3b59b258a4eb5c51e83611f02bf37ac16cacd
Vladislav Mararenko <mararenko.vladislav@gmail.com>
2022-12-06 20:06:51 +0200

    infinite scroll draft

--- src/index.js
+++ src/index.js
@@ -8,30 +8,42 @@ const refs = {
   searchForm: document.querySelector('.search-form'),
   loadMoreBtn: document.querySelector('.load-more'),
   galleryWrap: document.querySelector('.gallery'),
+  end: document.querySelector('.end-line'),
 };
 
 const galleryService = new GalleryService();
+
+const intersectOptions = {
+  rootMargin: '0px',
+  threshold: 1.0,
+};
 
 const clearMarkup = () => {
   refs.galleryWrap.innerHTML = '';
 };
 
-const showLoadMoreBtn = () => {
-  refs.loadMoreBtn.classList.remove('hidden');
+const showEndLine = () => {
+  refs.end.classList.remove('hidden');
 };
 
-const hideLoadMoreBtn = () => {
-  refs.loadMoreBtn.classList.add('hidden');
+const hideEndLine = () => {
+  refs.end.classList.add('hidden');
 };
 
 const notify = photosObj => {
   const { totalHits, hits } = photosObj;
 
-  if (galleryService.page === galleryService.totalPages) {
+  if (galleryService.totalPages === 1) {
+    hideEndLine();
+  }
+
+  if (
+    galleryService.page === galleryService.totalPages &&
+    galleryService.totalPages != 1
+  ) {
     Notiflix.Notify.info(
       `We're sorry, but you've reached the end of search results.`
     );
-    hideLoadMoreBtn();
   }
 
   if (galleryService.page === 1 && totalHits != 0) {
@@ -42,7 +54,7 @@ const notify = photosObj => {
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
-    hideLoadMoreBtn();
+    hideEndLine();
     return;
   }
 };
@@ -80,39 +92,52 @@ const render = photos => {
 
 const onFormSubmit = e => {
   e.preventDefault();
-  hideLoadMoreBtn();
+
+  hideEndLine();
   clearMarkup();
   galleryService.resetPages();
 
   const query = e.target.elements.searchQuery.value.trim();
+
+  if (!query) {
+    return;
+  }
 
   galleryService.currentQuery = query;
 
   galleryService.fetchGallery().then(r => {
     galleryService.totalPages = Math.ceil(r.data.totalHits / 40);
 
-    showLoadMoreBtn();
+    showEndLine();
     render(r.data);
     notify(r.data);
   });
 };
 
-const onLoadMoreClick = () => {
-  galleryService.currentPage += 1;
+const onPageEnd = e => {
+  const isPageEnded = e[0].isIntersecting;
 
-  galleryService.fetchGallery().then(r => {
-    render(r.data);
-    notify(r.data);
-    const { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
+  if (isPageEnded) {
+    galleryService.currentPage += 1;
 
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
+    galleryService.fetchGallery().then(r => {
+      render(r.data);
+      notify(r.data);
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
     });
-  });
+  }
 };
+
+const observer = new IntersectionObserver(onPageEnd, intersectOptions);
+
+observer.observe(refs.end);
 
 refs.searchForm.addEventListener('submit', onFormSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
